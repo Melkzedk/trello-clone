a8640eb8d1e4e3aea16f5571c761e0a4c1e21625
Melkzedk <melkywafula29@gmail.com>
2026-04-14 09:33:32 +0300

    small changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,27 @@
-import from 'lodash/fp';
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
-Test Test;
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={{
+            padding: 10,
+            margin: "0 0 8px 0",
+            backgroundColor: "#fff",
+            borderRadius: 4,
+            ...provided.draggableProps.style,
+          }}
+        >
+          {task.content}
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
