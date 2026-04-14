0909e53c8af28492b14025ddc460fcdcfd5ab3fe
Melkzedk <melkywafula29@gmail.com>
2026-04-14 09:33:59 +0300

    changes

---
+++ src/components/Column.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Droppable } from "react-beautiful-dnd";
+import Task from "./Task";
+
+const Column = ({ column, tasks }) => {
+  return (
+    <div style={{ margin: 8 }}>
+      <h3>{column.title}</h3>
+
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            style={{
+              backgroundColor: "#f4f5f7",
+              padding: 8,
+              width: 250,
+              minHeight: 100,
+            }}
+          >
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
+export default Column;
