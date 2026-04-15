eb248c0471d0db1815623338230d997521bf12a7
Melkzedk <melkywafula29@gmail.com>
2026-04-15 09:42:55 +0300

    small changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,6 @@ const Task = ({ task, index }) => {
             margin: "0 0 8px 0",
             backgroundColor: "#fff",
             borderRadius: 4,
-            textShadow: "0 1px 0 rgba(9,30,66,.25)",
             ...provided.draggableProps.style,
           }}
         >
