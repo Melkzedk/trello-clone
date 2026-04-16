39dc8e183963c1dd3f1ac1c786d072997e7dd2e9
Melkzedk <melkywafula29@gmail.com>
2026-04-16 08:18:27 +0300

    small changes

---
+++ src/components/Task.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Draggable } from "@hello-pangea/dnd";
+
+const Task = ({ task, index }) => {
+  return (
+    <Draggable draggableId={task.id} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={{
+            padding: 10,
+            margin: "0 0 8px 0",
+            backgroundColor: "#fff",
+            borderRadius: 4,
+            ...provided.draggableProps.style,
+          }}
+        >
+          {task.content}
+        </div>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
